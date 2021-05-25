b6bf2c0e691f82dfc522515ccd10811cab3c3784
jarvis <soluciones.zapata@gmail.com>
2021-05-24 19:50:06 -0600

    created class Padre

--- src/com/ucreativa/Main.java
+++ src/com/ucreativa/Main.java
@@ -1,12 +1,13 @@
 package com.ucreativa;
 
-import com.ucreativa.poc.Adolfo;
+import com.ucreativa.familia.Adolfo;
+import com.ucreativa.familia.Padre;
 
 public class Main {
 
     public static void main(String[] args) {
 	    Adolfo  estudiante = new Adolfo();
-
+        Padre hector = new Padre("Padre: Hola Hector");
 
     }
 }
---
+++ src/com/ucreativa/familia/Padre.java
@@ -0,0 +1,11 @@
+package com.ucreativa.familia;
+
+/*
+    Clase nombre del padre
+ */
+public class Padre {
+
+    public Padre(String saludo){
+        System.out.println("" + saludo + "");
+    }
+}
