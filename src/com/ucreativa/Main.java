67c1d37511ef9c4ff41e8744ff132f11314271d0
jarvis <soluciones.zapata@gmail.com>
2021-05-24 20:42:52 -0600

    created class Madre with Edad

--- src/com/ucreativa/Main.java
+++ src/com/ucreativa/Main.java
@@ -1,6 +1,7 @@
 package com.ucreativa;
 
 import com.ucreativa.familia.Adolfo;
+import com.ucreativa.familia.Madre;
 import com.ucreativa.familia.Padre;
 
 public class Main {
@@ -8,6 +9,11 @@ public class Main {
     public static void main(String[] args) {
 	    Adolfo  estudiante = new Adolfo();
         Padre hector = new Padre("Padre: Hola Hector");
+        Madre mercedes = new Madre();
+
+        mercedes.setAge();
+        System.out.println("Edad de madre: " + mercedes.getAge());
+
 
     }
 }
