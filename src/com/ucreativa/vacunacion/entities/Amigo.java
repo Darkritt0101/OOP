3eeeb6fe945fc0e87b735332ffe313b8e2284a00
jarvis <soluciones.zapata@gmail.com>
2021-06-03 01:16:29 -0600

    updated class amigo and Familiar

--- src/com/ucreativa/vacunacion/entities/Amigo.java
+++ src/com/ucreativa/vacunacion/entities/Amigo.java
@@ -2,31 +2,13 @@ package com.ucreativa.vacunacion.entities;
 
 public class Amigo extends Persona {
 
-    private String nombre;
-    private String cedula;
-    private int edad;
-    private boolean riesgo;
     private String relacion;
     private String facebbok;
 
-    public Amigo(String nombre, String cedula, int edad, boolean riesgo) {
+    public Amigo(String nombre, String cedula, int edad, boolean riesgo, String relacion, String facebbok) {
         super(nombre, cedula, edad, riesgo);
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public String getCedula() {
-        return cedula;
-    }
-
-    public int getEdad() {
-        return edad;
-    }
-
-    public boolean isRiesgo() {
-        return riesgo;
+        this.relacion = relacion;
+        this.facebbok = facebbok;
     }
 
     public String getRelacion() {
@@ -35,13 +17,5 @@ public class Amigo extends Persona {
 
     public String getFacebbok() {
         return facebbok;
-    }
-
-    public void setEdad(int edad) {
-        this.edad = edad + 1;
-    }
-
-    public void setRiesgo(boolean riesgo) {
-        this.riesgo = riesgo;
     }
 }
--- src/com/ucreativa/vacunacion/entities/Familiar.java
+++ src/com/ucreativa/vacunacion/entities/Familiar.java
@@ -2,42 +2,14 @@ package com.ucreativa.vacunacion.entities;
 
 public class Familiar extends Persona{
 
-    private String nombre;
-    private String cedula;
-    private int edad;
-    private boolean riesgo;
     private String parentesco;
 
-    public Familiar(String nombre, String cedula, int edad, boolean riesgo) {
+    public Familiar(String nombre, String cedula, int edad, boolean riesgo, String parentesco) {
         super(nombre, cedula, edad, riesgo);
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public String getCedula() {
-        return cedula;
-    }
-
-    public int getEdad() {
-        return edad;
-    }
-
-    public boolean isRiesgo() {
-        return riesgo;
+        this.parentesco = parentesco;
     }
 
     public String getParentesco() {
         return parentesco;
     }
-
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }
-
-    public void setRiesgo(boolean riesgo) {
-        this.riesgo = riesgo;
-    }
-
 }
